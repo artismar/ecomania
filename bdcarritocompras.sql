fb0ebf184c259c78ddf7fec3d4c39df455bf3217
Lucas Martin <lucasezequielmartin@hotmail.com>
2021-12-03 03:46:49 -0300

    Base datos act

--- bdcarritocompras.sql
+++ bdcarritocompras.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 15-10-2018 a las 23:12:45
--- Versión del servidor: 10.1.34-MariaDB
--- Versión de PHP: 7.2.7
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-12-2021 a las 07:44:56
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,9 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `compra` (
   `idcompra` bigint(20) NOT NULL,
-  `cofecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `cofecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `idusuario` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`idcompra`, `cofecha`, `idusuario`) VALUES
+(45, '2021-12-03 05:38:00', 1),
+(46, '2021-12-03 06:24:14', 1),
+(47, '2021-12-03 06:32:14', 13);
 
 -- --------------------------------------------------------
 
@@ -44,9 +52,18 @@ CREATE TABLE `compraestado` (
   `idcompraestado` bigint(20) UNSIGNED NOT NULL,
   `idcompra` bigint(11) NOT NULL,
   `idcompraestadotipo` int(11) NOT NULL,
-  `cefechaini` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cefechaini` timestamp NOT NULL DEFAULT current_timestamp(),
   `cefechafin` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `compraestado`
+--
+
+INSERT INTO `compraestado` (`idcompraestado`, `idcompra`, `idcompraestadotipo`, `cefechaini`, `cefechafin`) VALUES
+(8, 45, 3, '2021-12-03 05:38:00', '2021-12-03 05:38:39'),
+(9, 46, 1, '2021-12-03 06:24:14', '0000-00-00 00:00:00'),
+(10, 47, 2, '2021-12-03 06:32:14', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -83,6 +100,16 @@ CREATE TABLE `compraitem` (
   `cicantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `compraitem`
+--
+
+INSERT INTO `compraitem` (`idcompraitem`, `idproducto`, `idcompra`, `cicantidad`) VALUES
+(30, 28, 45, 1),
+(31, 29, 45, 5),
+(32, 29, 46, 1),
+(33, 29, 47, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -94,7 +121,7 @@ CREATE TABLE `menu` (
   `menombre` varchar(50) NOT NULL COMMENT 'Nombre del item del menu',
   `medescripcion` varchar(124) NOT NULL COMMENT 'Descripcion mas detallada del item del menu',
   `idpadre` bigint(20) DEFAULT NULL COMMENT 'Referencia al id del menu que es subitem',
-  `medeshabilitado` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha en la que el menu fue deshabilitado por ultima vez'
+  `medeshabilitado` timestamp NULL DEFAULT current_timestamp() COMMENT 'Fecha en la que el menu fue deshabilitado por ultima vez'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -102,11 +129,17 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`idmenu`, `menombre`, `medescripcion`, `idpadre`, `medeshabilitado`) VALUES
-(7, 'nuevo', 'kkkkk', NULL, NULL),
-(8, 'nuevo', 'kkkkk', NULL, NULL),
-(9, 'nuevo', 'kkkkk', 7, NULL),
-(10, 'nuevo', 'kkkkk', NULL, NULL),
-(11, 'nuevo', 'kkkkk', NULL, NULL);
+(1, 'Inicio', 'index.php', NULL, NULL),
+(2, 'Productos', 'productos.php', NULL, NULL),
+(3, 'Contacto', '#', NULL, NULL),
+(4, 'Administracion', 'administracion.php', NULL, NULL),
+(5, 'Deposito', 'deposito.php', NULL, NULL),
+(6, 'Cuenta', 'cuenta.php', NULL, NULL),
+(7, 'Editar perfil', 'editPerfil.php', 6, NULL),
+(8, 'Gestion productos', 'listaProductos.php', 5, NULL),
+(13, 'Pedidos', 'verPedidos.php', 6, NULL),
+(14, 'Gestion Menus', 'listaMenu.php', 4, NULL),
+(41, 'Gestión Pedidos', 'gestionarPedidos.php', 4, NULL);
 
 -- --------------------------------------------------------
 
@@ -119,6 +152,24 @@ CREATE TABLE `menurol` (
   `idrol` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `menurol`
+--
+
+INSERT INTO `menurol` (`idmenu`, `idrol`) VALUES
+(4, 1),
+(5, 2),
+(6, 1),
+(6, 2),
+(6, 3),
+(7, 1),
+(7, 2),
+(7, 3),
+(8, 2),
+(13, 3),
+(14, 1),
+(41, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -127,10 +178,24 @@ CREATE TABLE `menurol` (
 
 CREATE TABLE `producto` (
   `idproducto` bigint(20) NOT NULL,
-  `pronombre` int(11) NOT NULL,
-  `prodetalle` varchar(512) NOT NULL,
-  `procantstock` int(11) NOT NULL
+  `pronombre` varchar(50) NOT NULL,
+  `prodetalle` varchar(1000) NOT NULL,
+  `procantstock` int(11) NOT NULL,
+  `proprecio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idproducto`, `pronombre`, `prodetalle`, `procantstock`, `proprecio`) VALUES
+(28, 'Cepillo de dientes', 'Los cepillo de dientes Meraki son Biodegradables y tienen la misma vida útil y calidad de lavado que los cepillos convencionales.\r\n\r\n97% Biodegradable: El bambú es una de las plantas que más rápido crece en el mundo. Tiene propiedades antibacterianas. No requiere riego artificial, pesticidas ni fertilizantes.\r\n\r\n3% Reciclable: 31 Cerdas de Nylon reciclables. Con 3 tipos de dureza a elección: (suave, media y dura).\r\n\r\n¿Cual es la vida útil de los cepillos?\r\nLas cerdas tienen la misma vida útil que los cepillos de dientes convencionales, se recomienda cambiarlos cada 3 meses. El bambú del mango puede perdurar por años cuando se almacena en un lugar seco.\r\n\r\nCuando te cepillas los dientes 3 veces por día, recordás con felicidad que reducís el impacto ambiental: pequeños hábitos, grandes cambios.', 0, 250),
+(29, 'Esponja vegetal', 'Las esponjas vegetales largas, son 100% biodegradables, veganas y ecológicas; tras su utilización pueden volver al compost, ademas son de alta calidad como exfoliantes.\r\n\r\nSe emplea en múltiples usos, tanto para el cuidado personal como para el lavado de vajilla, fabricación de cepillos, etc.\r\n\r\nEs probable que encuentres semillas en su interior, al brotar crece como una enredadera y tendrás muchos frutos para regalar.\r\n\r\nConsejo de uso y cuidado\r\n\r\nAclararla después de cada uso y dejar secar al aire.\r\n\r\n \r\n\r\nGuardar en un lugar seco.\r\n\r\n \r\n\r\nPara eliminar eventuales bacterias, se recomienda, cada cierto tiempo, meter la esponja o el estropajo en el microondas entre 10 y 20 segundos en un recipiente con agua\r\n\r\n \r\n\r\nEn caso de cambiar de color o de producir olor, reemplazarla\r\n\r\n \r\n\r\nAntes de utilizar la esponja humedécela, verás que ya no es tan rígida. Una vez húmeda realiza suaves movimientos circulares sobre la piel.\r\n\r\n \r\n\r\nTambién recomendamos dejarlo sumergido durante una noche ', 3, 200),
+(30, 'Sorbete acero', 'Pajita de Acero rectas de color plateado de excelente calidad. Dimensiones: 20 cm de largo, 0,7 cm de ancho.\r\nIndustria argentina: son preferibles en reemplazo de los sorbetes importados (menor impacto de CO2 por ausencia de flete marítimo).\r\n\r\n¿Te imaginas la cantidad de pajitas plásticas que evitarás en tu vida? Comenzá a disfrutar el cuidado del planeta, con cada trago.', 38, 100),
+(31, 'Botella Meraki', 'Reemplazás el consumo de agua embotellada por el buen hábito de tener una botella reutilizable que te acompañe.\r\n\r\nCaracteristicas:\r\n\r\nBotella térmica de 600ml.\r\n20hs de bebida fría.\r\n10hs de bebida caliente.\r\n5 colores diferentes.\r\nDoble capa de acero inoxidable.\r\nTapa a rosca de acero con terminaciones de bambú.', 0, 2500),
+(32, 'Bee wraps', 'Bee wraps o envoltorios de cera de abeja, el reemplazo del papel film.\r\n\r\nConverti? tu casa en una eco amigable, sirve para cubrir bowls, quesos, verduras, frutas, panes y mucho ma?s.\r\nCon el calor de tus manos vas moldeando las formas alrededor de la comida.\r\n\r\nSon eco amigables, biodegradables, antibacterial, lavables y reutilizables. Duran entre 6 y 12 meses.\r\n\r\nVienen de 4 tamaños diferentes', 0, 450),
+(33, 'Rasuradora clasica', 'Como las primeras maquinas de acero, duran para toda la vida tal que ahorras mas que comprando afeitadoras descartables, y, ademas contribuis con el medio ambiente.\r\n\r\nRasuradora metálica reutilizable para afeitado femenino y masculino. Es de acero y viene en una caja plástica útil para trasladar en viajes sin riesgos de corte, ademas cuenta con un espejo en su interior. Si no la necesitas, podés pedirnos que la reciclemos por vos.\r\n\r\nIncluye:\r\n1 Estuche con espejo.\r\n1 Rasuradora.\r\n1 Hoja de afeitar.\r\n1 Cepillito.', 20, 1500),
+(35, 'asdeqw', 'nuevo', 10, 300);
 
 -- --------------------------------------------------------
 
@@ -143,6 +208,15 @@ CREATE TABLE `rol` (
   `rodescripcion` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `rodescripcion`) VALUES
+(1, 'Administrador'),
+(2, 'Deposito'),
+(3, 'Cliente');
+
 -- --------------------------------------------------------
 
 --
@@ -152,10 +226,18 @@ CREATE TABLE `rol` (
 CREATE TABLE `usuario` (
   `idusuario` bigint(20) NOT NULL,
   `usnombre` varchar(50) NOT NULL,
-  `uspass` int(11) NOT NULL,
+  `uspass` varchar(50) NOT NULL,
   `usmail` varchar(50) NOT NULL,
   `usdeshabilitado` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `usnombre`, `uspass`, `usmail`, `usdeshabilitado`) VALUES
+(1, 'Lucas', '0192023a7bbd73250516f069df18b500', 'admin@ecomania.com', '0000-00-00 00:00:00'),
+(13, 'Claudio', '7159bbe0c8ca2a67230a26b72dea7557', 'cliente@ecomania.com', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -167,6 +249,16 @@ CREATE TABLE `usuariorol` (
   `idusuario` bigint(20) NOT NULL,
   `idrol` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuariorol`
+--
+
+INSERT INTO `usuariorol` (`idusuario`, `idrol`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(13, 3);
 
 --
 -- Índices para tablas volcadas
@@ -238,7 +330,8 @@ ALTER TABLE `rol`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idusuario`),
-  ADD UNIQUE KEY `idusuario` (`idusuario`);
+  ADD UNIQUE KEY `idusuario` (`idusuario`),
+  ADD UNIQUE KEY `usmail` (`usmail`);
 
 --
 -- Indices de la tabla `usuariorol`
@@ -256,43 +349,43 @@ ALTER TABLE `usuariorol`
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `idcompra` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `idcompra` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT de la tabla `compraestado`
 --
 ALTER TABLE `compraestado`
-  MODIFY `idcompraestado` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idcompraestado` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `compraitem`
 --
 ALTER TABLE `compraitem`
-  MODIFY `idcompraitem` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idcompraitem` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT de la tabla `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `idmenu` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idmenu` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idproducto` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `idproducto` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `idrol` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `idrol` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `idusuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Restricciones para tablas volcadas
